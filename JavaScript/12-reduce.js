69aec506477f21d99f736fa8a703b6204dc3246c
Professor SESI/SENAI <Professor@SN-353296.edu.fiesc.com.br>
2025-10-23 20:49:56 -0300

    atualização 2

--- JavaScript/12-reduce.js
+++ JavaScript/12-reduce.js
@@ -5,3 +5,39 @@ const soma = numeros.reduce(
 )
 
 console.log("A soma é " + soma)
+
+// ====================================
+
+const multi = numeros.reduce(
+    (ac, num) => ac * num, 1
+)
+
+console.log("A multiplicação é " + multi)
+
+// ====================================
+
+const notas = [7, 8, 6, 5]
+
+const media = notas.reduce(
+    (ac, nota, index, array) => {
+        //ac: acumulador; nota: variável de iteração
+        //index: índice da lista, array: nome da lista
+        ac += nota;
+        if(index === array.length -1){
+            return ac / array.length
+        }
+        return ac;
+    }
+)
+
+console.log(media)
+
+// ====================================
+
+const palavras = ["Olá", "mundo", "JavaScript"]
+
+const frase = palavras.reduce(
+    (ac, p) => `${ac} ${p}`
+)
+
+console.log(frase)
